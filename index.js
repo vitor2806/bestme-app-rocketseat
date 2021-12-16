e07379b912aeedc23d5792fa9b99b06c7ffc1813
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-16 13:59:01 -0300

    add stdin.on for data inserting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //process.stdout.write("Something \n") //escrever algo de saída, é o que o console.log faz por debaixo dos panos, adicionando o \n ao final da linha
 
 const questions = [
-    "What I've learned today?",
+    "What've I learned today?",
     "What've get me angry?",
     "What should I do to become better?",
     "What've get me happy today?",
@@ -13,3 +13,7 @@ const ask = function (index = 0) {
 }
 
 ask()
+
+process.stdin.on("data", data => { //.on fica aguardando a inserção de um novo dado
+    process.exit() //process.stdout.write(data.toString().trim()) //toString converte o dado para String e trim() remove os espaçamentos antes ou depois da string
+})
